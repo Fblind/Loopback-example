1dac83dda99cbd74c95dd1e6e08a1b2c4b4d9816
Facundo Soria <facundo.soria@devecoop.com>
2015-07-14 11:51:26 -0300

    Update models with ACLs

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,9 @@
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    console.log('beforeRemote');
+    var req = context.req;
+    req.body.date = Date.now();
+    req.body.publisherId = req.accessToken.userId;
+    next();
+  });
 };
